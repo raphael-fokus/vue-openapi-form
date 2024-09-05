d32e4b92153021ef56a09fb12cbfa889fd5d97af
Raphael Koch <raphael.koch@fokus.fraunhofer.de>
2024-09-05 14:22:54 +0200

    ..

--- dist/assets/size.9cc64778.js
+++
@@ -1,1 +0,0 @@
-import{d as e}from"./index.d81025d2.js";var s=e({data(){return{isMedium:!1}},mounted(){document.querySelector(".vue-openapi-form").classList.contains("is-medium")&&(this.isMedium=!0)}});export{s};
