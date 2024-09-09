c9148b55244ccbc935f9b8d5d390c7901c6d1e35
Raphael Koch <raphael.koch@fokus.fraunhofer.de>
2024-09-09 16:09:07 +0200

    worker request path fixed

--- src/components/WorkerList.vue
+++ src/components/WorkerList.vue
@@ -1,61 +1,60 @@
 <template>
-    <div>
-      <h2>Available Workers</h2>
-      <div v-if="workers.length">
-        <div v-for="worker in workers" :key="worker.workerId" class="listEntry">
-          {{ worker.workerType }}: {{ worker.workerName }} 
-          <i>({{ worker.workerId }} {{ worker.connected ? '✔️' : '⚠️' }})</i>
-          <input type="text" v-model="refSetting[worker.workerId]" placeholder="refSettingId" />
-          <button @click="assignWorker(worker)" class="button is-primary">Assign</button>
-        </div>
-      </div>
-      <div v-else>
-        No workers available.
+  <div>
+    <h2>Available Workers</h2>
+    <div v-if="workers.length">
+      <div v-for="worker in workers" :key="worker.workerId" class="listEntry">
+        {{ worker.workerType }}: {{ worker.workerName }} 
+        <i>({{ worker.workerId }} {{ worker.connected ? '✔️' : '⚠️' }})</i>
+        <input type="text" v-model="refSetting[worker.workerId]" placeholder="refSettingId" />
+        <button @click="assignWorker(worker)" class="button is-primary">Assign</button>
       </div>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    props: {
-      selectedJob: {
-        type: Object,
-        required: false // Only show when a job is selected
-      }
-    },
-    data() {
-      return {
-        workers: [],
-        refSetting: {} // To store reference settings for workers
-      };
-    },
-    mounted() {
-      this.fetchWorkers();
-    },
-    methods: {
-      fetchWorkers() {
-        this.$axios.get("http://localhost:3003/v1/workers")
-          .then(response => {
-            this.workers = response.data;
-          })
-          .catch(error => {
-            console.error("Error fetching workers:", error);
-          });
-      },
-      assignWorker(worker) {
-        if (!this.selectedJob) {
-          alert('Please select a job first');
-          return;
-        }
-  
-        const refSettingId = this.refSetting[worker.workerId] || "";
-        this.$emit('assign-worker', { worker, refSettingId });
-      }
-    }
-  };
-  </script>
-  
-  <style scoped>
+    <div v-else>
+      No workers available.
+    </div>
+  </div>
+</template>
 
-  </style>
-  
+<script>
+export default {
+  props: {
+    selectedJob: {
+      type: Object,
+      required: false // Only show when a job is selected
+    }
+  },
+  data() {
+    return {
+      workers: [],
+      refSetting: {} // To store reference settings for workers
+    };
+  },
+  mounted() {
+    this.fetchWorkers();
+  },
+  methods: {
+    fetchWorkers() {
+      this.$axios.get("http://localhost:3003/v1/registration")
+        .then(response => {
+          this.workers = response.data;
+        })
+        .catch(error => {
+          console.error("Error fetching workers:", error);
+        });
+    },
+    assignWorker(worker) {
+      if (!this.selectedJob) {
+        alert('Please select a job first');
+        return;
+      }
+
+      const refSettingId = this.refSetting[worker.workerId] || "";
+      this.$emit('assign-worker', { worker, refSettingId });
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
