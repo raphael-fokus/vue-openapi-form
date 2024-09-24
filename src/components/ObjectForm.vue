6468927b64ab9e57fe368945d3d4d9409affe5c9
Raphael Koch <raphael.koch@fokus.fraunhofer.de>
2024-09-24 12:23:42 +0200

    Dropdown if schema contains Enum

--- src/components/ObjectForm.vue
+++ src/components/ObjectForm.vue
@@ -1,96 +1,54 @@
+<!-- object-form.vue -->
 <template>
   <div class="mb-15" :class="{ 'is-hidden': isSelfFolded }">
-    <template v-for="(key, idx) in Object.keys(properties)">
-      <!-- if the property is another object -->
-      <v-field
-        v-if="properties[key].type === 'object'"
-        :key="key + '-object'"
-        v-slot="{ field, handleChange }"
-        v-model="modelData[key]"
-        :name="`${fieldName}/${key}`"
-        :rules="ruleObject(propertiesRequired && isRequired(key))"
-        :label="`${properties[key].title}`"
-        as=""
-      >
-        <object-form-wrapper
-          :model-value="field.value"
-          :field-name="`${fieldName}/${key}`"
-          :expand-form="level < 2"
-          :is-last-child="idx === Object.keys(properties).length - 1"
-          :level="level + 1"
-          :type="properties[key].type"
-          :is-self-required="propertiesRequired && isRequired(key)"
-          :schema="properties[key]"
-          :errors="errors"
-          :reference-model="referenceModel[key] || {}"
-          :showRootTab="false"
-          @update:modelValue="handleChange"
-        />
+    <template v-for="(key, idx) in Object.keys(properties)" :key="key">
+      <!-- If the property is another object -->
+      <v-field v-if="properties[key].type === 'object'" :key="key + '-object'" v-slot="{ field, handleChange }"
+        v-model="modelData[key]" :name="`${fieldName}/${key}`"
+        :rules="ruleObject(propertiesRequired && isRequired(key))" :label="properties[key].title || key" as="">
+        <object-form-wrapper :model-value="field.value" :field-name="`${fieldName}/${key}`" :expand-form="level < 2"
+          :is-last-child="idx === Object.keys(properties).length - 1" :level="level + 1" :type="properties[key].type"
+          :is-self-required="propertiesRequired && isRequired(key)" :schema="properties[key]" :errors="errors"
+          :reference-model="referenceModel[key] || {}" :showRootTab="false" @update:modelValue="handleChange" />
       </v-field>
-      <!-- if the property is additional property (key-value-pairs) -->
-      <v-field
-        v-else-if="properties[key].type === 'key-value-pairs'"
-        :key="key + '-key-value-pairs'"
-        v-slot="{ field, handleChange }"
-        v-model="modelData[key]"
-        :name="`${fieldName}/${key}`"
-        :rules="ruleObject(propertiesRequired && isRequired(key))"
-        :label="`${properties[key].title}`"
-        as=""
-      >
-        <key-value-pairs
-          :model-value="field.value"
-          :field-name="`${fieldName}/${key}`"
-          :is-last-child="idx === Object.keys(properties).length - 1"
-          :type="properties[key].type"
-          :schema="properties[key]"
-          :errors="errors"
-          :reference-model="referenceModel[key] || {}"
-          @update:modelValue="handleChange"
-        />
+
+      <!-- If the property is a key-value pair -->
+      <v-field v-else-if="properties[key].type === 'key-value-pairs'" :key="key + '-key-value-pairs'"
+        v-slot="{ field, handleChange }" v-model="modelData[key]" :name="`${fieldName}/${key}`"
+        :rules="ruleObject(propertiesRequired && isRequired(key))" :label="properties[key].title || key" as="">
+        <key-value-pairs :model-value="field.value" :field-name="`${fieldName}/${key}`"
+          :is-last-child="idx === Object.keys(properties).length - 1" :type="properties[key].type"
+          :schema="properties[key]" :errors="errors" :reference-model="referenceModel[key] || {}"
+          @update:modelValue="handleChange" />
       </v-field>
-      <!-- if the property is array -->
-      <v-field
-        v-else-if="properties[key].type === 'array'"
-        :key="key + '-array'"
-        v-slot="{ field, handleChange }"
-        v-model="modelData[key]"
-        :name="`${fieldName}/${key}`"
-        :rules="ruleArray(propertiesRequired && isRequired(key))"
-        :label="`${properties[key].title}`"
-        as=""
-      >
-        <array-input
-          :model-value="field.value"
-          :field-name="`${fieldName}/${key}`"
-          :is-last-child="idx === Object.keys(properties).length - 1"
-          :type="properties[key].type"
-          :schema="properties[key]"
-          :errors="errors"
-          :reference-model="referenceModel[key] || []"
-          @update:modelValue="handleChange"
-        />
+
+      <!-- If the property is an array -->
+      <v-field v-else-if="properties[key].type === 'array'" :key="key + '-array'" v-slot="{ field, handleChange }"
+        v-model="modelData[key]" :name="`${fieldName}/${key}`" :rules="ruleArray(propertiesRequired && isRequired(key))"
+        :label="properties[key].title || key" as="">
+        <array-input :model-value="field.value" :field-name="`${fieldName}/${key}`"
+          :is-last-child="idx === Object.keys(properties).length - 1" :type="properties[key].type"
+          :schema="properties[key]" :errors="errors" :reference-model="referenceModel[key] || []"
+          @update:modelValue="handleChange" />
       </v-field>
-      <!-- if the property is simple string, number -->
-      <v-field
-        v-else
-        :key="key"
-        v-slot="{ field, handleChange, errors: fieldErrors, meta }"
-        v-model="modelData[key]"
-        :rules="ruleString(propertiesRequired && isRequired(key))"
-        :name="`${fieldName}/${key}`"
-        :label="`${properties[key].title}`"
-        as=""
-      >
-        <simple-input
-          :key="key"
-          :model-value="field.value"
-          :type="properties[key].type"
-          :schema="properties[key]"
-          :validation-ob="{ errors: fieldErrors, ...meta }"
-          :reference-model="referenceModel[key] || ''"
-          @update:modelValue="handleChange"
-        />
+
+      <!-- If the property is an enum (handled by SelectInput.vue) -->
+      <v-field v-else-if="properties[key].enum" :key="key + '-enum'"
+        v-slot="{ field, handleChange, errors: fieldErrors, meta }" v-model="modelData[key]"
+        :name="`${fieldName}/${key}`" :rules="ruleString(propertiesRequired && isRequired(key))"
+        :label="properties[key].title || key" as="">
+        <select-input :model-value="field.value" :schema="properties[key]"
+          :validation-ob="{ errors: fieldErrors, ...meta }" :reference-model="referenceModel[key] || ''"
+          @update:modelValue="handleChange" />
+      </v-field>
+
+      <!-- If the property is a simple string or number -->
+      <v-field v-else :key="key + '-simple'" v-slot="{ field, handleChange, errors: fieldErrors, meta }"
+        v-model="modelData[key]" :rules="ruleString(propertiesRequired && isRequired(key))"
+        :name="`${fieldName}/${key}`" :label="properties[key].title || key" as="">
+        <simple-input :key="key" :model-value="field.value" :type="properties[key].type" :schema="properties[key]"
+          :validation-ob="{ errors: fieldErrors, ...meta }" :reference-model="referenceModel[key] || ''"
+          @update:modelValue="handleChange" />
       </v-field>
     </template>
   </div>
@@ -101,10 +59,21 @@ import { model } from '../mixins/model.js';
 import fold from '../mixins/fold.js';
 import validation from '../mixins/validation.js';
 import { defineComponent } from 'vue';
+import SimpleInput from './SimpleInput.vue';
+import SelectInput from './SelectInput.vue';
+import ObjectFormWrapper from './ObjectFormWrapper.vue';
+import KeyValuePairs from './KeyValuePairs.vue';
+import ArrayInput from './ArrayInput.vue';
 
 export default defineComponent({
   name: 'ObjectForm',
-
+  components: {
+    SimpleInput,
+    SelectInput,
+    ObjectFormWrapper,
+    KeyValuePairs,
+    ArrayInput,
+  },
   mixins: [model, fold, validation],
   props: {
     properties: {
@@ -135,19 +104,51 @@ export default defineComponent({
       type: Number,
       default: 1,
     },
+    referenceModel: {
+      type: Object,
+      default: () => ({}),
+    },
+    isSelfRequired: {
+      type: Boolean,
+      default: false,
+    },
+    isSelfFolded: {
+      type: Boolean,
+      default: false,
+    },
   },
-
   computed: {
-    // for calculating if the property is actually required or not (Based on parent object self requirement)
     propertiesRequired() {
       return this.isSelfRequired || Object.keys(this.modelData).length > 0;
     },
+    modelData: {
+      get() {
+        return this.modelValue;
+      },
+      set(value) {
+        this.$emit('update:modelValue', value);
+      },
+    },
   },
-
   methods: {
     isRequired(key) {
-      const item = this.required.find((itm) => itm === key);
-      return item ? true : false;
+      return this.required.includes(key);
+    },
+    ruleEnum(isRequired) {
+      // Implement your validation rules for enums here
+      return isRequired ? 'required' : '';
+    },
+    ruleObject(isRequired) {
+      // Implement your validation rules for objects here
+      return isRequired ? 'required' : '';
+    },
+    ruleArray(isRequired) {
+      // Implement your validation rules for arrays here
+      return isRequired ? 'required' : '';
+    },
+    ruleString(isRequired) {
+      // Implement your validation rules for strings here
+      return isRequired ? 'required' : '';
     },
   },
 });
---
+++ src/components/SelectInput.vue
@@ -0,0 +1,203 @@
+<!-- SelectInput.vue -->
+<template>
+    <div class="ac-single-input is-small is-information">
+      <label
+        :class="[labelShow ? 'show-label' : '', 'ac-label']"
+        @click.prevent="focusInput()"
+      >
+        {{ schema.title }}
+      </label>
+      <div class="select-wrapper">
+        <select
+          ref="selectField"
+          v-model="modelData"
+          class="ac-input"
+          :class="{
+            'is-success': validationOb.dirty && validationOb.valid,
+            'is-danger': validationOb.dirty && !validationOb.valid,
+            'bg-white': modelData,
+          }"
+          @focus="triggerInput()"
+          @blur="unTriggerInput()"
+        >
+          <option v-if="!modelData" value="" disabled>Select an option</option>
+          <option
+            v-for="option in schema.enum"
+            :key="option"
+            :value="option"
+          >
+            {{ option }}
+          </option>
+        </select>
+        <!-- Validation icons -->
+        <template v-if="validationOb.dirty">
+          <span v-if="validationOb.valid" class="validation-icon is-success">
+            <i class="fa fa-check"></i>
+          </span>
+          <span v-if="!validationOb.valid" class="validation-icon is-danger">
+            <i class="fa fa-exclamation-triangle"></i>
+          </span>
+        </template>
+        <!-- Error message -->
+        <p v-if="validationOb.errors.length > 0" class="help is-danger">
+          {{ validationOb.errors[0] }}
+        </p>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import { model } from '../mixins/model.js';
+  import validation from '../mixins/validation.js';
+  import size from '../mixins/size.js';
+  import { defineComponent } from 'vue';
+  
+  export default defineComponent({
+    name: 'SelectInput',
+  
+    mixins: [model, validation, size],
+  
+    props: {
+      schema: {
+        type: Object,
+        default: () => ({}),
+      },
+      modelValue: {
+        type: [String, Number, null],
+        default: '',
+      },
+      validationOb: {
+        type: Object,
+        default: () => ({}),
+      },
+    },
+    emits: ['update:modelValue'],
+  
+    data() {
+      return {
+        labelShow: !!this.modelValue,
+      };
+    },
+  
+    computed: {
+      identifier() {
+        return `id-${this.schema.title.replace(/\s+/g, '-')}-${Date.now()}`;
+      },
+      modelData: {
+        get() {
+          return this.modelValue;
+        },
+        set(value) {
+          this.$emit('update:modelValue', value);
+        },
+      },
+    },
+  
+    methods: {
+      triggerInput() {
+        this.labelShow = true;
+      },
+      unTriggerInput() {
+        if (!this.modelData) this.labelShow = false;
+      },
+      focusInput() {
+        this.labelShow = true;
+        this.$refs.selectField.focus();
+      },
+    },
+  });
+  </script>
+  
+  <style scoped>
+  .ac-single-input {
+    position: relative;
+    margin-bottom: 20px;
+  }
+  
+  .ac-label {
+    position: absolute;
+    top: 12px;
+    left: 12px;
+    font-size: 12px;
+    color: #aaa;
+    transition: all 0.2s;
+  }
+  
+  .ac-input {
+    width: 100%;
+    padding: 12px;
+    padding-left: 12px;
+    padding-right: 40px;
+    font-size: 14px;
+    border: 1px solid #ccc;
+    border-radius: 4px;
+  }
+  
+  .ac-input:focus {
+    outline: none;
+    border-color: #3273dc;
+  }
+  
+  .show-label {
+    top: -4px;
+    font-size: 12px;
+    color: #3273dc;
+  }
+  
+  .validation-icon {
+    position: absolute;
+    right: 12px;
+    top: 50%;
+    transform: translateY(-50%);
+    font-size: 18px;
+  }
+  
+  .validation-icon.is-success {
+    color: #23d160;
+  }
+  
+  .validation-icon.is-danger {
+    color: #ff3860;
+  }
+  
+  .help.is-danger {
+    color: #ff3860;
+    margin-top: 5px;
+  }
+  
+  .select-wrapper {
+    position: relative;
+  }
+  
+  .select-wrapper select.ac-input {
+    appearance: none;
+    -moz-appearance: none;
+    -webkit-appearance: none;
+    background-color: #fff;
+    background-image: url('data:image/svg+xml;utf8,<svg fill="%23999" height="7" viewBox="0 0 10 7" width="10" xmlns="http://www.w3.org/2000/svg"><path d="M0 0l5 7 5-7z"/></svg>');
+    background-repeat: no-repeat;
+    background-position: right 10px center;
+    background-size: 10px 7px;
+  }
+  
+  .select-wrapper select.ac-input {
+    padding-right: 50px;
+  }
+  
+  .select-wrapper .validation-icon {
+    right: 30px; 
+  }
+  
+  .select-wrapper select.ac-input.is-success {
+    border-color: #23d160;
+  }
+  
+  .select-wrapper select.ac-input.is-danger {
+    border-color: #ff3860;
+  }
+  
+  .bg-white {
+    background-color: #fff;
+  }
+  </style>
+  
